d38fae1d8fdcc48e0d006cea6625e576bac49fab
Siddu Prabhu <sidduprabhu.yu18@gmail.com>
2023-08-30 01:23:18 +0530

    Data Ingestion

--- src/logger.py
+++ src/logger.py
@@ -2,6 +2,7 @@ import logging
 import os
 from datetime import datetime
 
+
 LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 logs_path = os.path.join(os.getcwd(),"logs",LOG_FILE)
 os.makedirs(logs_path,exist_ok=True)
@@ -13,3 +14,5 @@ logging.basicConfig(
     format="[%(asctime)s] %(lineno)d %(name)s - %(lovelname)s - %(message)s",
     level=logging.INFO
 )
+
+
